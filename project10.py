16ccde4b7cc57ef48b52bef56c2e2949897acef4
DipayanCoder25 <dipayanprogramer3@gmail.com>
2025-05-28 17:48:32 +0600

    Add files via upload

--- project10.py
+++ project10.py
@@ -1,7 +1,14 @@
-import calendar
+def make_sqaure(start, end):
+    squares = [i**2 for i in range(start, end + 1)]
+    
+    even_squares = [sq for sq in squares if sq % 2 == 0]
+    odd_squares = [sq for sq in squares if sq % 2 != 0]
+    
+    print("Even square values:")
+    print(even_squares)
+    
+    print("\nOdd square values:")
+    print(odd_squares)
 
-
-months = list(calendar.month_name)
-
-for month in months[1:]:
-    print(month)
+# Example usage:
+make_sqaure(1, 10)
