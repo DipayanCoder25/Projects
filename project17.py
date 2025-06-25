0b6fa7bf8275abf3eaab1d7122e81c1d04102947
DipayanCoder25 <dipayanprogramer3@gmail.com>
2025-06-25 18:36:30 +0600

    Add files via upload

---
+++ project17.py
@@ -0,0 +1,14 @@
+class Reverse:
+    def __init__(self, s=""):
+        self.s = s
+
+    def get_reversed(self):
+        return self.s[::-1]
+
+
+user_input = input("Enter a word: ")
+obj = Reverse(user_input)
+
+
+print("Reversed word:", obj.get_reversed())
+
