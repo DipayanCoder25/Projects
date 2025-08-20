c1e5b27f339242c6a2ad1ebd03ba574664cd07ec
DipayanCoder25 <dipayanprogramer3@gmail.com>
2025-08-20 20:17:22 +0600

    Add files via upload

--- project25.py
+++ project25.py
@@ -12,16 +12,15 @@ def convert():
 root = Tk()
 root.title("Inches to Centimeters Converter")
 root.geometry("400x400")
-# Input field
+
 Label(root, text="Enter length in inches:").pack(padx=10,pady=5)
 entry = Entry(root)
 entry.pack(pady=5)
 
-# Convert button
 convert_btn = Button(root, text="Convert", command=convert)
 convert_btn.pack(pady=5)
 
-# Result label
+
 result_label = Label(root, text="")
 result_label.pack(pady=5)
 
